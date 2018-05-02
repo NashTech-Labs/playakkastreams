5b4ce4d5abac032ea25f7f88b4a8f9bd2bb63efb
Sidharth Khattri <mavericksid@users.noreply.github.com>
2018-05-02 16:50:14 +0530

    added information about close-delimited and chunked response

--- app/controllers/CustomerController.scala
+++ app/controllers/CustomerController.scala
@@ -12,19 +12,24 @@ import play.api.mvc._
 class CustomerController @Inject()(cc: ControllerComponents,
                                    customerRepository: CustomerRepository) extends AbstractController(cc) {
 
-  def customers: Action[AnyContent] = Action { implicit request =>
-    val customerDatabasePublisher = customerRepository.customers
-    val customerSource = Source.fromPublisher(customerDatabasePublisher)
+def customers: Action[AnyContent] =
+    Action { implicit request =>
+      val customerDatabasePublisher = customerRepository.customers
+      val customerSource = Source.fromPublisher(customerDatabasePublisher)
 
-    val headerCSVSource = Source.single(ByteString(""""First Name","Last Name","Email"""" + "\n"))
-    val customerCSVSource =
-      customerSource.map(data => ByteString(s""""${data.firstName}","${data.lastName}","${data.email}"""" + "\n"))
+      val headerCSVSource = Source.single(ByteString(""""First Name","Last Name","Email"""" + "\n"))
+      val customerCSVSource =
+        customerSource.map(data => ByteString(s""""${data.firstName}","${data.lastName}","${data.email}"""" + "\n"))
 
-    val csvSource = Source.combine(headerCSVSource, customerCSVSource)(Concat[ByteString])
+      val csvSource = Source.combine(headerCSVSource, customerCSVSource)(Concat[ByteString])
 
-    Result(
-      header = ResponseHeader(OK, Map(CONTENT_DISPOSITION → s"attachment; filename=customers.csv")),
-      body = HttpEntity.Streamed(csvSource, None, None))
-  }
+      // Chunked transfer encoding is only supported for HTTP/1.1
+      // Ok.chunked(csvSource)
+
+      // Sends the content as close-delimited which enables backward compatibility with HTTP/1.0
+      Result(
+        header = ResponseHeader(OK, Map(CONTENT_DISPOSITION → s"attachment; filename=customers.csv")),
+        body = HttpEntity.Streamed(csvSource, None, None))
+    }
 
 }
