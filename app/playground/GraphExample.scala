b2374791b46fcc40bb77539baf02ab99ec84e91b
sidharth <sidharth.khattri@gmail.com>
2018-05-13 19:03:28 +0530

    reformatted graph example class

--- app/playground/GraphExample.scala
+++ app/playground/GraphExample.scala
@@ -20,22 +20,22 @@ class GraphExample {
   // Runnable graph - NotUsed print to console
   // ===================================================================================================================
   val closedShapeGraphNotUsed: Graph[ClosedShape, NotUsed] =
-  GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
-    import GraphDSL.Implicits._
+    GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
 
-    val in = Source(List(1, 1, 1))
-    val out = Sink.foreach(println)
+      val in = Source(List(1, 1, 1))
+      val out = Sink.foreach(println)
 
-    val bcast: UniformFanOutShape[Int, Int] = builder.add(Broadcast[Int](2))
-    val merge: UniformFanInShape[Int, Int] = builder.add(Merge[Int](2))
+      val bcast: UniformFanOutShape[Int, Int] = builder.add(Broadcast[Int](2))
+      val merge: UniformFanInShape[Int, Int] = builder.add(Merge[Int](2))
 
-    val f1, f2, f3, f4 = Flow[Int].map(_ + 10)
+      val f1, f2, f3, f4 = Flow[Int].map(_ + 10)
 
-    in ~> f1 ~> bcast ~> f2 ~> merge ~> f3 ~> out
-    bcast ~> f4 ~> merge
+      in ~> f1 ~> bcast ~> f2 ~> merge ~> f3 ~> out
+      bcast ~> f4 ~> merge
 
-    ClosedShape
-  }
+      ClosedShape
+    }
 
   val runnableGraph1: RunnableGraph[NotUsed] = RunnableGraph.fromGraph(closedShapeGraphNotUsed)
   runnableGraph1.run()
@@ -51,20 +51,20 @@ class GraphExample {
 
   val doubleSinkClosedShapeGraph: Graph[ClosedShape, (Future[Int], Future[Int])] =
     GraphDSL
-    .create(topHeadSink, bottomHeadSink)((_, _)) { implicit builder: GraphDSL.Builder[(Future[Int], Future[Int])] =>
-      (topHS, bottomHS) =>
-        import GraphDSL.Implicits._
+      .create(topHeadSink, bottomHeadSink)((_, _)) { implicit builder: GraphDSL.Builder[(Future[Int], Future[Int])] =>
+        (topHS, bottomHS) =>
+          import GraphDSL.Implicits._
 
-        val source = Source.single(1)
-        val broadcast: UniformFanOutShape[Int, Int] = builder.add(Broadcast[Int](2))
+          val source = Source.single(1)
+          val broadcast: UniformFanOutShape[Int, Int] = builder.add(Broadcast[Int](2))
 
-        source ~> broadcast
+          source ~> broadcast
 
-        broadcast ~> sharedDoubler ~> topHS
-        broadcast ~> sharedDoubler ~> bottomHS
+          broadcast ~> sharedDoubler ~> topHS
+          broadcast ~> sharedDoubler ~> bottomHS
 
-        ClosedShape
-    }
+          ClosedShape
+      }
 
   val runnableGraph2: RunnableGraph[(Future[Int], Future[Int])] = RunnableGraph.fromGraph(doubleSinkClosedShapeGraph)
   val (future1, future2) = runnableGraph2.run()
@@ -80,17 +80,17 @@ class GraphExample {
   // Runnable graph - with 3 inputs and one output which finds the max of 3 inputs
   // ===================================================================================================================
   val pickMaxOfThree: Graph[UniformFanInShape[Int, Int], NotUsed] =
-  GraphDSL.create() { implicit b =>
-    import GraphDSL.Implicits._
+    GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
 
-    // ZipWith's last type is output
-    val zip1: FanInShape2[Int, Int, Int] = b.add(ZipWith[Int, Int, Int](math.max))
-    val zip2: FanInShape2[Int, Int, Int] = b.add(ZipWith[Int, Int, Int](math.max))
+      // ZipWith's last type is output
+      val zip1: FanInShape2[Int, Int, Int] = b.add(ZipWith[Int, Int, Int](math.max))
+      val zip2: FanInShape2[Int, Int, Int] = b.add(ZipWith[Int, Int, Int](math.max))
 
-    zip1.out ~> zip2.in0
+      zip1.out ~> zip2.in0
 
-    UniformFanInShape(zip2.out, zip1.in0, zip1.in1, zip2.in1)
-  }
+      UniformFanInShape(zip2.out, zip1.in0, zip1.in1, zip2.in1)
+    }
 
   val resultSink: Sink[Int, Future[Int]] = Sink.head[Int]
 
@@ -118,19 +118,19 @@ class GraphExample {
   // Runnable graph - 2 sources zipped into tuple
   // ===================================================================================================================
   val pairsSource: Source[(Int, Int), NotUsed] =
-  Source.fromGraph(GraphDSL.create() { implicit b =>
-    import GraphDSL.Implicits._
+    Source.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
 
-    val zip: FanInShape2[Int, Int, (Int, Int)] = b.add(Zip[Int, Int]())
+      val zip: FanInShape2[Int, Int, (Int, Int)] = b.add(Zip[Int, Int]())
 
-    def ints: Source[Int, NotUsed] =
-      Source.fromIterator(() => Iterator.from(1))
+      def ints: Source[Int, NotUsed] =
+        Source.fromIterator(() => Iterator.from(1))
 
-    ints.filter(_ % 2 != 0) ~> zip.in0
-    ints.filter(_ % 2 == 0) ~> zip.in1
+      ints.filter(_ % 2 != 0) ~> zip.in0
+      ints.filter(_ % 2 == 0) ~> zip.in1
 
-    SourceShape(zip.out)
-  })
+      SourceShape(zip.out)
+    })
 
   val firstPair: (Int, Int) = Await.result(pairsSource.runWith(Sink.head), Duration.Inf)
   println(">>>>>>> firstPair " + firstPair)
@@ -156,17 +156,17 @@ class GraphExample {
   // Runnable graph - Int to (Int, String)
   // ===================================================================================================================
   val pairUpWithToString: Flow[Int, (Int, String), NotUsed] =
-  Flow.fromGraph(GraphDSL.create() { implicit b =>
-    import GraphDSL.Implicits._
+    Flow.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
 
-    val bcast: UniformFanOutShape[Int, Int] = b.add(Broadcast[Int](2))
-    val zip: FanInShape2[Int, String, (Int, String)] = b.add(Zip[Int, String]())
+      val bcast: UniformFanOutShape[Int, Int] = b.add(Broadcast[Int](2))
+      val zip: FanInShape2[Int, String, (Int, String)] = b.add(Zip[Int, String]())
 
-    bcast.out(0) ~> zip.in0
-    bcast.out(1).map(_.toString) ~> zip.in1
+      bcast.out(0) ~> zip.in0
+      bcast.out(1).map(_.toString) ~> zip.in1
 
-    FlowShape(bcast.in, zip.out)
-  })
+      FlowShape(bcast.in, zip.out)
+    })
 
   val (_, future) = pairUpWithToString.runWith(Source.single(1), Sink.head)
   val r4: (Int, String) = Await.result(future, Duration.Inf)
@@ -232,12 +232,12 @@ class GraphExample {
   // Accessing materialized value inside the Graph
   // ===================================================================================================================
   val foldFlow: Flow[Int, Int, Future[Int]] =
-  Flow.fromGraph(GraphDSL.create(Sink.fold[Int, Int](0)(_ + _)) { implicit b: GraphDSL.Builder[Future[Int]] =>
-    fold: SinkShape[Int] =>
-      import GraphDSL.Implicits._
+    Flow.fromGraph(GraphDSL.create(Sink.fold[Int, Int](0)(_ + _)) { implicit b: GraphDSL.Builder[Future[Int]] =>
+      fold: SinkShape[Int] =>
+        import GraphDSL.Implicits._
 
-      FlowShape(fold.in, b.materializedValue.mapAsync(4)(identity).outlet)
-  })
+        FlowShape(fold.in, b.materializedValue.mapAsync(4)(identity).outlet)
+    })
 
   val runnableGraph4: RunnableGraph[Future[Int]] =
     Source(List(1, 2)).viaMat(foldFlow)(Keep.right).to(Sink.foreach(e => println(s">>>>>>> foldFlow $e")))
@@ -247,19 +247,19 @@ class GraphExample {
   // Graph cycle - deadlock - all processing stops after some time
   // ===================================================================================================================
   val deadlockRunnableGraph: RunnableGraph[NotUsed] =
-  RunnableGraph.fromGraph(GraphDSL.create() { implicit b: GraphDSL.Builder[NotUsed] =>
-    import GraphDSL.Implicits._
+    RunnableGraph.fromGraph(GraphDSL.create() { implicit b: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
 
-    val merge = b.add(Merge[Int](2))
-    val bcast = b.add(Broadcast[Int](2))
+      val merge = b.add(Merge[Int](2))
+      val bcast = b.add(Broadcast[Int](2))
 
-    val source = Source.fromIterator(() => Iterator.from(1))
+      val source = Source.fromIterator(() => Iterator.from(1))
 
-    source ~> merge ~> Flow[Int].map { s => println(s"Feedback graph with deadlock $s"); s } ~> bcast ~> Sink.ignore
-    merge <~ bcast
+      source ~> merge ~> Flow[Int].map { s => println(s"Feedback graph with deadlock $s"); s } ~> bcast ~> Sink.ignore
+      merge <~ bcast
 
-    ClosedShape
-  })
+      ClosedShape
+    })
 
   // deadlockRunnableGraph.run()
 
@@ -268,19 +268,19 @@ class GraphExample {
   //               source remain back-pressured forever.
   // ===================================================================================================================
   val notDeadlockRunnableGraph: RunnableGraph[NotUsed] =
-  RunnableGraph.fromGraph(GraphDSL.create() { implicit b: GraphDSL.Builder[NotUsed] =>
-    import GraphDSL.Implicits._
+    RunnableGraph.fromGraph(GraphDSL.create() { implicit b: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
 
-    val merge = b.add(MergePreferred[Int](1))
-    val bcast = b.add(Broadcast[Int](2))
+      val merge = b.add(MergePreferred[Int](1))
+      val bcast = b.add(Broadcast[Int](2))
 
-    val source = Source.fromIterator(() => Iterator.from(1))
+      val source = Source.fromIterator(() => Iterator.from(1))
 
-    source ~> merge ~> Flow[Int].map { s => println(s"Feedback graph without deadlock $s"); s } ~> bcast ~> Sink.ignore
-    merge.preferred <~ bcast
+      source ~> merge ~> Flow[Int].map { s => println(s"Feedback graph without deadlock $s"); s } ~> bcast ~> Sink.ignore
+      merge.preferred <~ bcast
 
-    ClosedShape
-  })
+      ClosedShape
+    })
 
   // notDeadlockRunnableGraph.run()
 
@@ -289,19 +289,19 @@ class GraphExample {
   //               elements are continuously printed on the console hence not constantly back-pressured.
   // ===================================================================================================================
   val droppingHeadRunnableGraph: RunnableGraph[NotUsed] =
-  RunnableGraph.fromGraph(GraphDSL.create() { implicit b: GraphDSL.Builder[NotUsed] =>
-    import GraphDSL.Implicits._
+    RunnableGraph.fromGraph(GraphDSL.create() { implicit b: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
 
-    val merge = b.add(Merge[Int](2))
-    val bcast = b.add(Broadcast[Int](2))
+      val merge = b.add(Merge[Int](2))
+      val bcast = b.add(Broadcast[Int](2))
 
-    val source = Source.fromIterator(() => Iterator.from(1))
+      val source = Source.fromIterator(() => Iterator.from(1))
 
-    source ~> merge ~> Flow[Int].map { s => println(s"Feedback graph with dropping head $s"); s } ~> bcast ~> Sink.ignore
-    merge <~ Flow[Int].buffer(10, OverflowStrategy.dropHead) <~ bcast
+      source ~> merge ~> Flow[Int].map { s => println(s"Feedback graph with dropping head $s"); s } ~> bcast ~> Sink.ignore
+      merge <~ Flow[Int].buffer(10, OverflowStrategy.dropHead) <~ bcast
 
-    ClosedShape
-  })
+      ClosedShape
+    })
 
   // droppingHeadRunnableGraph.run()
 
@@ -310,20 +310,20 @@ class GraphExample {
   //               from feedback arc. But this does not output anything on the console because no right is present
   // ===================================================================================================================
   val zippedRunnableGraph: RunnableGraph[NotUsed] =
-  RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
-    import GraphDSL.Implicits._
+    RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
 
-    val zip = b.add(ZipWith[Int, Int, Int]((left, right) => right))
-    val bcast = b.add(Broadcast[Int](2))
+      val zip = b.add(ZipWith[Int, Int, Int]((left, right) => right))
+      val bcast = b.add(Broadcast[Int](2))
 
-    val source = Source.fromIterator(() => Iterator.from(1))
+      val source = Source.fromIterator(() => Iterator.from(1))
 
-    source ~> zip.in0
-    zip.out.map { s => println(s"Zipped feedback graph $s"); s } ~> bcast ~> Sink.ignore
-    zip.in1 <~ bcast
+      source ~> zip.in0
+      zip.out.map { s => println(s"Zipped feedback graph $s"); s } ~> bcast ~> Sink.ignore
+      zip.in1 <~ bcast
 
-    ClosedShape
-  })
+      ClosedShape
+    })
 
   // zippedRunnableGraph.run()
 
@@ -332,42 +332,24 @@ class GraphExample {
   //               from feedback arc. Unlike above the console prints elements as the stream was initiated.
   // ===================================================================================================================
   val zippedRunnableGraphWithStart: RunnableGraph[NotUsed] =
-  RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
-    import GraphDSL.Implicits._
+    RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
+      import GraphDSL.Implicits._
 
-    val zip = b.add(ZipWith[Int, Int, Int]((left, right) => right))
-    val bcast = b.add(Broadcast[Int](2))
-    val concat = b.add(Concat[Int]())
+      val zip = b.add(ZipWith[Int, Int, Int]((left, right) => right))
+      val bcast = b.add(Broadcast[Int](2))
+      val concat = b.add(Concat[Int]())
 
-    val start = Source.single(1)
-    val source = Source.fromIterator(() => Iterator.from(2))
+      val start = Source.single(1)
+      val source = Source.fromIterator(() => Iterator.from(2))
 
-    source ~> zip.in0
-    zip.out.map { s => println(s"Zipped feedback graph with start $s"); s } ~> bcast ~> Sink.ignore
-    zip.in1 <~ concat <~ start
-    concat <~ bcast
+      source ~> zip.in0
+      zip.out.map { s => println(s"Zipped feedback graph with start $s"); s } ~> bcast ~> Sink.ignore
+      zip.in1 <~ concat <~ start
+      concat <~ bcast
 
-    ClosedShape
-  })
+      ClosedShape
+    })
 
   // zippedRunnableGraphWithStart.run()
-
-  // ===================================================================================================================
-  // Running stages asynchronously
-  // ===================================================================================================================
-  Source(1 to 3)
-  .map { i => println(s"A sync: $i"); i }
-  .map { i => println(s"B sync: $i"); i }
-  .map { i => println(s"C sync: $i"); i }
-  .runWith(Sink.ignore)
-
-  println
-  println
-
-  Source(1 to 3)
-  .map { i => println(s"A async: $i"); i }.async
-  .map { i => println(s"B async: $i"); i }.async
-  .map { i => println(s"C async: $i"); i }.async
-  .runWith(Sink.ignore)
 
 }
